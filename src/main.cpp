8df148f953b77606d6e643fc74be474f2dad5dea
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-10-19 01:29:54 -0300

    Codigo finalizado

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,16 @@
 #include <Arduino.h>
 
 //Variaveis
-int sensor = A0;
-int valor;
+int sensor = A0; //Variavel sensor no pino A0
+int valor; //Variavel para valor do sensor
 
 void setup() {
-    pinMode(sensor, INPUT);
+    pinMode(sensor, INPUT); //Pino sensor como entrada
+    Serial.begin(9600); //Inicia comunicação serial
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    valor = analogRead(sensor); //Valor recebe a leitura do sensor
+    Serial.println(valor); //Exibe o valor da leitura
+    delay(3000); //Espera 3 segundos
 }
